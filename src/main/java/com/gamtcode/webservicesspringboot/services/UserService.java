b5c4a8afc63ca82e74c5fc1ef65eaf8c992ea359
Guilherme Teixeira <guilhermemancilha@gmail.com>
2024-05-17 10:52:22 -0300

    Exception handling - delete

--- src/main/java/com/gamtcode/webservicesspringboot/services/UserService.java
+++ src/main/java/com/gamtcode/webservicesspringboot/services/UserService.java
@@ -1,9 +1,12 @@
 package com.gamtcode.webservicesspringboot.services;
 
 import com.gamtcode.webservicesspringboot.entities.User;
+import com.gamtcode.webservicesspringboot.services.exceptions.DatabaseException;
 import com.gamtcode.webservicesspringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.gamtcode.webservicesspringboot.repositories.UserRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            if (repository.existsById(id)) {
+                repository.deleteById(id);
+            } else {
+                throw new ResourceNotFoundException(id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
